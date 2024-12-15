28279ea450be56011992022a377d69e3aa77af8d
New name <Ei@gami>
2024-12-15 12:37:46 +0100

    newcommit

--- python.py
+++ python.py
@@ -35,17 +35,50 @@ def K2_2K (list , index1 , index2):
 def K2_1K (list , index1 , index2) :
     list1 = [0,1,2,3]
     list1.remove(index1)
+    list1.remove(index2) 
+    list[list1[0]] , list[list1[1]] =  list[list1[1]] , list[list1[0]]
+    K3_0K( list , index1, index2 )
+    
+def K3_0K (list,index1,index2):
+    list1 = [0,1,2,3]
+    list1.remove(index1)
     list1.remove(index2)
+    value =  list[list1[1]]
+    list[list1[1]]=5
+    print(list)
+    x = int(input(x))
+    y = int(input(y))
+    if(x == 2 and y == 1):
+        list[list1 [0]]=5
+        list[list1 [1]]=value
+    if(x == 2 and y == 0):
+        list[list1[0]]=6
+        list[list1 [1]]=value
+    if(x==3):
+        list[list1[1]]=6
+            
+    print(list) 
+# def K1_1 ( list , index1 ,index2 ) :
+#     list1 = [0,1,2,3]
+#     value2 = index1
+#     list1.remove(index1)
+#     value = index2
+#     list1.remove(index2)
+#     list[list1[value]] , list[list1[1]] =  list[list1[1]] , list[list1[value]]
+#     print(list)
+#     x = input()
+#     y = input()
+#     if x == 1 : 
+#         list1 = [0,1,2,3]
+#         list1 = [  ]
+        
+        
     
     
-
+    
+       
 Startlist = [1,2,3,4]
-
-
-while (True): 
-    help(Startlist)
-    
-
+   
 
 
 # print(Startlist)
@@ -64,6 +97,7 @@ if x == 2 and y == 0 :
 if x == 0 and y == 2:
     Startlist = [2,1,3,4]
     help(Startlist)
+    print(x)
     if x == 0 : 
         Startlist = [2,1,4,3]
         print(Startlist)
@@ -80,4 +114,3 @@ if x == 0 and y == 2:
      
     
  
-print(K2_0K([1,4,2,3] , 0 , 1))
