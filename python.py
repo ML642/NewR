1bbb96ff3da4cb2a63937cf35c84c427a53f2e6a
New name <Ei@gami>
2024-12-15 13:12:29 +0100

    new

--- python.py
+++ python.py
@@ -1,8 +1,20 @@
 # [1,2,3,4,5,6]
+def check(ciag,index1,index2):
+            print(Startlist)
+            x = int(input())
+            y = int(input())
+            if x == 2 and y == 0 :
+                print(K2_0K(ciag,index1,index2))
+            if x == 2 and y == 1 :
+                print(K2_1K(ciag,index1,index2))
+            if x == 2 and y == 2:
+                print(K2_2K(ciag,index1,index2)) 
+
 def help(ciag) :
+    print(ciag)
     x = int(input())
     y = int(input())
-    print(ciag)
+    
 def K2_0K (list , index1 , index2) :
      list1 = [0,1,2,3]
      list1.remove(index1)
@@ -45,7 +57,7 @@ def K3_0K (list,index1,index2):
     list1.remove(index2)
     value =  list[list1[1]]
     list[list1[1]]=5
-    print(list)
+    
     x = int(input(x))
     y = int(input(y))
     if(x == 2 and y == 1):
@@ -71,46 +83,26 @@ def K3_0K (list,index1,index2):
 #     if x == 1 : 
 #         list1 = [0,1,2,3]
 #         list1 = [  ]
-        
-        
-    
-    
-    
-       
 Startlist = [1,2,3,4]
-   
-
-
-# print(Startlist)
-
+print(Startlist)
 # print("ile podanych cyfr jest na dobrym miejscu")
-
 x = int(input())
-
-# print(" ile podanych cyfr znajduje sie w kodzie , ale nie jest na dobrym mejscu ")
-
-
+# print(" ile podanych cyfr znajduje sie w kodzie , ale nie jest na dobrym mejscu "
 y = int(input())
-if x == 2 and y == 0 :
-    K2_0K(Startlist ,  )
 
 if x == 0 and y == 2:
     Startlist = [2,1,3,4]
-    help(Startlist)
-    print(x)
+    check(Startlist,0,1)
     if x == 0 : 
         Startlist = [2,1,4,3]
-        print(Startlist)
-        x = int(input())
-        y = int(input())
+        check(Startlist,2,3)
         if x == 0 :
             Startlist = [2,4,1,3]
             print(Startlist)
-            x = int(input())
-            y = int(input())
+            check(Startlist,1,2)
             if x == 0 : 
                 Startlist = [3,4,1,2]
-                print()    
+                print(K2_0K(Startlist,0,3))    
      
     
  
